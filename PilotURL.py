e46148dcede7955db85b98fa6661db269352491f
RodrigoVillar <77309055+RodrigoVillar@users.noreply.github.com>
2021-01-11 20:44:52 -0500

    Add files via upload

---
+++ PilotURL.py
@@ -0,0 +1,165 @@
+"""
+PilotURL.py is in charge of initializing the various URL classes and retrieving
+the links/downloading files that Pilot.py requests. PilotURL.py can only recieve
+information from Pilot.py and cannot access any attributes/methods of Pilot.py
+unless it is passed through
+
+- Rod
+"""
+
+import requests
+from bs4 import BeautifulSoup
+
+class MainURL(object): # There's a chance the class inheritance may need to be changed
+    """
+    This class is meant to be used as a base for current and future subclasses.
+    Since PilotsProgram will require many files/resources from the internet,
+    having just one main class work all the necessary operations of the program
+    is not feasable.
+
+    Since this is a helper module, all attributes are hidden.
+    """
+
+    # Hidden Attributes
+
+    # Attribute _ICAO:
+    # Invariant:
+
+    # Attribute _IATA:
+    # Invariant:
+
+    # Getters/Setters GO HERE
+    def getICAO(self):
+        """
+        Returns self._ICAO (the ICAO airport code)
+        """
+        return self._ICAO
+
+
+    def getIATA(self):
+        """
+        Returns self._IATA (the IATA airport code)
+        """
+        return self._IATA
+
+
+    def __init__(self, icao, iata):
+        """
+        When called, the initializer creates the attribute necessary for the
+        class (in this case, it is '_ICAO'). No preconditions need to be enforced
+        as Pilot.py verifies that code (the airport code the user has inputted)
+        corresponds to ICAO format.
+        """
+        self._ICAO = icao
+        self._IATA = iata
+
+
+class adURL(MainURL): # Inherits from MainURL class
+    """
+    This class is responsible for handling the process behind retrieving the
+    airport diagram of the airport the user has inputted.
+
+    This class inherits its initializer from MainURL, adds the _payload
+    attribute and (currently) contains the methods needed to retrieve (and
+    download, but that's a work in progress) the airport diagram from the
+    FAA's website
+    """
+
+    # Hidden Attributes
+
+    # Attribute _payload:
+    # Invariant:
+
+    # Getters/Setters GO HERE
+    def getPayload(self):
+        """
+        Returns self._payload, a dictionary that contains infomration used when making a
+        POST request to the FAA website for airport diagrams
+        """
+        return self._payload
+
+
+    def __init__(self, icao, iata):
+        """
+        When called, this initializer uses super to call the initializer for
+        MainURL. Afterwards, it creates the attribute _payload which is a
+        dictionary used for the POST request to the FAA website
+        """
+        super().__init__(icao, iata)
+        self._payload = {
+            'ident': self.getICAO(),
+            #state: # Commented out sections need a look at later.
+            #airport:
+            #'ver': '2013',
+            #'eff': '12-03-2020',
+            #'end': '12-31-2020',
+            'diagrams': '1', # For future improvements, changing the diagram number changes the diagram given by the URL
+            'cycle': '2013',
+        }
+
+
+    def adRetrieve(self):
+        """
+        When called, this procedure takes the attributes _ICAO and _IATA and uses it to
+        retrieve the corresponding airport diagram from the FAA's website
+        """
+        temp = requests.session()
+        response = temp.post('https://www.faa.gov/air_traffic/flight_info/aeronav/digital_products/dtpp/search/results/', data=self.getPayload()) # SHORTEN !!!
+        cleanResponse = response.text
+
+        soup = BeautifulSoup(cleanResponse, 'lxml')
+
+        result = ''
+        value = True
+        for link in soup.find_all('a'):
+            p = str(link.get('href'))
+            if p.find(self.getIATA()) > 0 and p.find('http://aeronav.faa.gov/') >= 0 and value:
+                result = p
+                print(result)
+                value = False
+
+        final = requests.get(str(result)) # This accesses the PDF of the airport diagram
+
+        fileName = str(self.getICAO()) + 'diagram.pdf'
+
+        #with open(fileName, 'wb') as f: # Uncomment if you want to download PDF
+        #    f.write(final.content)
+
+
+class metarsURL(MainURL):
+    """
+    This class is responsible for fetching the metars that pilots need prior
+    to flying. This class currently (only contain methods) and uses GET
+    requests to call to a website that contains metar information and parses
+    it using BeautifulSoup.
+    """
+    # Hidden Attributes Go Here
+
+    def metarsRetrieve(self):
+        """
+        When called, this procedure takes the attributes _ICAO and/or _IATA to
+        retrive the corresponding metars information from a website on the
+        internet.
+        """
+        temp = requests.session()
+        link = 'https://www.aviationweather.gov/metar/data?ids=' + str(self.getICAO()) +'&format=raw&date=&hours=0'
+        response = temp.get(link)
+        cleanResponse = response.text
+
+        soup = BeautifulSoup(cleanResponse, 'lxml')
+
+        result = ''
+        value = True
+        for info in soup.find_all('code'):
+            test = str(info)
+            if test.find(self.getICAO()) >= 0 and value:
+                result = info.text
+                value = False
+
+        self.metarsText = result
+        print(self.metarsText)
+
+
+class notamsURL(MainURL):
+
+    pass
